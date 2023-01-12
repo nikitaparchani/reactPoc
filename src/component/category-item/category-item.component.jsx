435f79a7e2c9183247648bb0714c5006bb42ff31
Nikita Parchani <nikitaparchani29@gmail.com>
2023-01-12 15:32:25 +0530

    updating our directory

---
+++ src/component/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+const CategoryItem = ({key , category}) =>{
+    return(
+<div key={key} className="category-container">
+          <div className="background-image" 
+          style={{
+            backgroundImage: `url(${category.imageUrl})`,
+          }}
+          />
+        <div className="category-body-container">
+          <h2>{category.title}</h2>
+          <p>Shop Now</p>
+      </div>
+    </div>
+    )
+        }
+export default CategoryItem;
